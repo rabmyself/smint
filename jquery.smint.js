2cd622922b686962d50b80277af97e50d2d123ad
Aziz Khambati <aziz.h.khambati@gmail.com>
2013-10-03 20:40:41 +0530

    Removed functions from .each loop
    Reduced the number of times the $(window).scroll & stickyMenu functions
    were being called by removing them from the .each loop and placing them
    outside.
    Each function was called 'n' times where 'n' is the number of
    $smintItems
    Also added a check for smint-disableAll for external links

--- jquery.smint.js
+++ jquery.smint.js
@@ -1,7 +1,7 @@
 /*
 
 SMINT V1.0 by Robert McCracken
-SMINT V2.0 by robert McCracken with some awesome help from Ryan Clarke (@clarkieryan) and mcpacosy ‏(@mcpacosy)
+SMINT V2.0 by robert McCracken with some awesome help from Ryan Clarke (@clarkieryan) and mcpacosy (@mcpacosy)
 
 SMINT is my first dabble into jQuery plugins!
 
@@ -12,118 +12,129 @@ If you like Smint, or have suggestions on how it could be improved, send me a tw
 */
 
 
-(function($){
+(function() {
 
-	$.fn.smint = function( options ) {
+	$.fn.smint = function(scrollSpeed) {
+
+		scrollSpeed = (typeof optionalArg === "undefined") ? 500 : scrollSpeed;
 
 		var $smint = this,
 			$smintItems = $smint.find('a'),
 			$window = $(window),
-			settings = $.extend({}, $.fn.smint.defaults, options),
-			// Set the variables needed
+			//Set the variables needed
 			optionLocs = [],
 			lastScrollTop = 0,
-			menuHeight = $smint.height();
+			menuHeight = $smint.height(),
+			curi = -1,
+			stickyTop = $smint.offset().top;
 
+		var stickyMenu = function(scrollingDown) {
+			// current distance top
+			var scrollTop = $(window).scrollTop();
 
-		return $smintItems.each( function(index) {
-            
+			// if we scroll more than the navigation, change its position to fixed and add class 'fxd', otherwise change it back to absolute and remove the class
+			if (scrollTop > stickyTop) {
+				//Check if he has scrolled horizontally also.
+				if ($(window).scrollLeft()) {
+					$smint.css({ 'position': 'fixed', 'top': 0, 'left': -$(window).scrollLeft() }).addClass('fxd');
+				}
+				else {
+					$smint.css({ 'position': 'fixed', 'top': 0, 'left': 'auto' }).addClass('fxd');
+				}
+			}
+			else {
+				$smint.css({ 'position': 'absolute', 'top': stickyTop, 'left': 'auto' }).removeClass('fxd');
+			}
+
+			// Check if the position is inside then change the menu
+			// Courtesy of Ryan Clarke (@clarkieryan)
+			if (!scrollingDown) {
+				while (true) {
+					if (scrollTop >= optionLocs[curi].top) {
+						$smintItems.removeClass('active');
+						$('#' + optionLocs[curi].id).addClass('active');
+						break;
+					}
+					curi++;
+					//Added as failsafe, should not be needed.
+					/*
+					if(curi > optionLocs.length) {
+						break;
+					}
+					*/
+				}
+			}
+			else {
+				while (true) {
+					if (scrollTop < optionLocs[curi].bottom) {
+						$smintItems.removeClass('active');
+						$('#' + optionLocs[curi].id).addClass('active');
+						break;
+					}
+					curi--;
+				}
+			}
+		};
+
+		// run function every time you scroll but not needed to be run for each of the $smintItems
+		$(window).scroll(function() {
+			//Get the direction of scroll
+			var st = $(this).scrollTop(),
+				scrollingDown = (st > lastScrollTop);
+			lastScrollTop = st;
+			stickyMenu(scrollingDown);
+
+			// Check if at bottom of page, if so, add class to last <a> as sometimes the last div
+			// isnt long enough to scroll to the top of the page and trigger the active state.
+			if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+				$smintItems.removeClass('active');
+				$smintItems.last().addClass('active');
+			}
+		});
+
+		$smintItems.each(function() {
+			if ($(this).hasClass("smint-disableAll")) {
+				return;
+			}
 			//Fill the menu
 			var id = this.id,
 				matchingSection = $("."+id),
 				sectionTop = matchingSection.position().top;
-
 			optionLocs.push({
-					top: matchingSection.position().top - menuHeight,
-					bottom: matchingSection.height() + sectionTop,
-					id: id
+				top: sectionTop - menuHeight,
+				bottom: matchingSection.height() + sectionTop - menuHeight,
+				id: id
 			});
-
-			///////////////////////////////////
-
-			// get initial top offset for the menu 
-			var stickyTop = $smint.offset().top;	
-
-			// check position and make sticky if needed
-			var stickyMenu = function(scrollingDown){
-
-				// current distance top
-				var scrollTop = $window.scrollTop();
-
-				// if we scroll more than the navigation, change its position to fixed and add class 'fxd', otherwise change it back to absolute and remove the class
-				if (scrollTop > stickyTop) { 
-					$smint.css({ 'position': 'fixed', 'top':0 }).addClass('fxd');	
-				} else {
-					$smint.css({ 'position': 'absolute', 'top':stickyTop }).removeClass('fxd'); 
-				}   
-
-				//Check if the position is inside then change the menu
-				// Courtesy of Ryan Clarke (@clarkieryan)
-
-
-				if(optionLocs[index].top <= scrollTop && scrollTop <= optionLocs[index].bottom){	
-					$smintItems.removeClass("active");
-					$("#"+id).addClass("active");
-					if(!scrollingDown){
-						if(index+1 < optionLocs.length) {
-							$("#"+optionLocs[index+1].id).removeClass("active");
-						}
-					} else if(index > 0) {
-						$("#"+optionLocs[index-1].id).removeClass("active");
-					}
-				}
-			};
-
-			// run function every time you scroll
-			$window = $(window);
-			$window.scroll(function() {
-				//Get the direction of scroll
-				var st = $(this).scrollTop(),
-					scrollingDown = (st > lastScrollTop), 
-					lastScrollTop = st;
-				stickyMenu(scrollingDown);
-
-				// Check if at bottom of page, if so, add class to last <a> as sometimes the last div
-				// isnt long enough to scroll to the top of the page and trigger the active state.
-
-				if($window.scrollTop() + $window.height() == $(document).height()) {
-					$smintItems.removeClass('active');
-					$smintItems.last().addClass('active');
-				}
-			});
-
-			///////////////////////////////////////
-    
-        
-        	$(this).click(function(e){
-				// gets the height of the menu. This is used for off-setting the scroll so the menu doesn't overlap any content in the section/div they just scrolled to
-				var selectorHeight = $smint.height();   
-
-        		// stops empty hrefs making the page jump when clicked
-				e.preventDefault();
-
-				// get id pf the button you just clicked
-		 		var id = this.id;
-
-		 		// if the link has the smint-disable class it will be ignored 
-		 		// Courtesy of mcpacosy ‏(@mcpacosy)
-
-                if ($(this).hasClass("smint-disable"))
-                {
-                    return false;
-                }
-
-				// gets the distance from top of the section/div class that matches your button id minus the height of the nav menu. This means the nav wont initially overlap the content.
-				var goTo =  $('.'+ id).offset().top -selectorHeight;
-
-				// Scroll the page to the desired position!
-				$("html, body").animate({ scrollTop: goTo }, settings.scrollSpeed);
-
-			});	
+			curi++;
 		});
+
+		//Just as a fail safe check to keep things sorted.
+		optionLocs.sort(function(a, b) {
+			if (a.top < b.top) {
+				return 1;
+			}
+			if (a.top > b.top) {
+				return -1;
+			}
+			return 0;
+		});
+
+		$smintItems.on('click', function(e) {
+			// if the link has the smint-disable class it will be ignored 
+			// Courtesy of mcpacosy(@mcpacosy)
+			if ($(this).hasClass("smint-disable") || $(this).hasClass("smint-disableAll")) {
+				return;
+			}
+			// stops empty hrefs making the page jump when clicked
+			// Added after the check of smint-disable so that if its an external href it will work.
+			e.preventDefault();
+			// gets the distance from top of the div class that matches your button id minus the height of the nav menu. This means the nav wont initially overlap the content.
+			// Scroll the page to the desired position!
+			$("html, body").animate({
+				scrollTop: ($('div.' + $(this).attr('id')).offset().top - menuHeight)
+			}, scrollSpeed);
+		});
+		// check position and make sticky if needed
+		$smintItems.first().addClass('active');
 	}
-
-	$.fn.smint.defaults = { 'scrollSpeed': 500};
-})(jQuery);
-
-
+})();
